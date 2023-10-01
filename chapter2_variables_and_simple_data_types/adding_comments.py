f4e960fb31955bb32b4c87d654afa59106e1874a
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-01 15:05:00 +0200

    01/10/23 15:04

---
+++ chapter2_variables_and_simple_data_types/adding_comments.py
@@ -0,0 +1,10 @@
+# Displays my favorite number.
+favorite_number = 8
+print(f"My favorite number is {favorite_number}")
+
+# Using different strip methods
+name = "\tMathis\t"
+print("'"+name+"'")
+print("'"+name.lstrip()+"'")
+print("'"+name.rstrip()+"'")
+print("'"+name.strip()+"'")
