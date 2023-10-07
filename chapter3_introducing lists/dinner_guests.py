1b460ad1198d909e96d6da0057693579996b785f
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-07 15:11:42 +0200

    07/10/23 15:11

---
+++ chapter3_introducing lists/dinner_guests.py
@@ -0,0 +1,15 @@
+guests = ["Alice", "Marc", "Claire", "Sam"]
+
+for guest in guests:
+    print(f"I'd like to invite you to dinner, {guest}!")
+
+print("Ayo I just found a bigger table!")
+
+guests.insert(0, "Mario")
+guests.insert(3, "Shrek")
+guests.append("God")
+
+for guest in guests:
+    print(f"I'd like to invite you to dinner, {guest}!")
+
+print(f"Number of people invited: {len(guests)}")
