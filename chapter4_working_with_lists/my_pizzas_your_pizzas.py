0134f77a1e80ed747cf3d17b26903ac0a295553f
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-14 16:54:17 +0200

    14/10/23 16:54

---
+++ chapter4_working_with_lists/my_pizzas_your_pizzas.py
@@ -0,0 +1,8 @@
+pizzas = ["Prosciutto", "Margherita", "Quatro Fromaggi"]
+friend_pizzas = pizzas[:]
+
+pizzas.append("Pepperoni")
+friend_pizzas.append("Hawaiian")
+
+print(f"My favorite pizzas are: {pizzas}")
+print(f"My friend's favorite pizzas are: {friend_pizzas}")
---
+++ chapter4_working_with_lists/slices.py
@@ -0,0 +1,6 @@
+cubes = [i**3 for i in range(1, 11)]
+print(cubes)
+
+print(f"The first three elements of the list are: {cubes[:3]}")
+print(f"The three elements in the middle of the list are: {cubes[4:7]}")
+print(f"The last three elements of the list are: {cubes[7:]}")
