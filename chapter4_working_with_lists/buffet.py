9eccf25766961ead50b8e9cfd218648e8bc248d5
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-14 16:59:54 +0200

    14/10/23 17:00

---
+++ chapter4_working_with_lists/buffet.py
@@ -0,0 +1,10 @@
+menu = ("Pizza", "Spaghetti", "Burger", "Grilled cheese", "Noodles")
+for item in menu:
+    print(item)
+
+# menu[3] = "Nuggets"
+
+menu = ("\nPizza", "Carbonara", "Burger", "Grilled cheese", "Nuggets")
+for item in menu:
+    print(item)
+
