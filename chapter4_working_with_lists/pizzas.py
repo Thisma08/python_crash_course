ae0121581165e9ff1b45c3380fa40966287360da
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-14 16:23:21 +0200

    14/10/23 16:23

---
+++ chapter4_working_with_lists/pizzas.py
@@ -0,0 +1,8 @@
+pizzas = ["Prosciutto", "Margherita", "Quatro Fromaggi"]
+for pizza in pizzas:
+    print(pizza)
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("I love pizza!")
