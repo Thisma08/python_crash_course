75c7d136bc62915d6db8b8cb5e46c6ba85febf54
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-21 14:35:06 +0200

    21/10/23 14:35

---
+++ chapter5_if_statements/alien_colors_2.py
@@ -0,0 +1,18 @@
+print("VERSION 1 (bloc if)")
+alien_color = "green"
+print(f"alien_color = {alien_color}")
+
+if alien_color == "green":
+    print("You earned 5 points.")
+else:
+    print("You earned 10 points.")
+
+print("\nVERSION 2 (bloc else)")
+alien_color = "red"
+print(f"alien_color = {alien_color}")
+
+
+if alien_color == "green":
+    print("You earned 5 points.")
+else:
+    print("You earned 10 points.")
---
+++ chapter5_if_statements/alien_colors_3.py
@@ -0,0 +1,32 @@
+print("VERSION 1")
+alien_color = "green"
+print(f"alien_color = {alien_color}")
+
+if alien_color == "green":
+    print("You earned 5 points.")
+elif alien_color == "yellow":
+    print("You earned 10 points.")
+elif alien_color == "red":
+    print("You earned 15 points.")
+
+print("\nVERSION 2")
+alien_color = "yellow"
+print(f"alien_color = {alien_color}")
+
+if alien_color == "green":
+    print("You earned 5 points.")
+elif alien_color == "yellow":
+    print("You earned 10 points.")
+elif alien_color == "red":
+    print("You earned 15 points.")
+
+print("\nVERSION 3")
+alien_color = "red"
+print(f"alien_color = {alien_color}")
+
+if alien_color == "green":
+    print("You earned 5 points.")
+elif alien_color == "yellow":
+    print("You earned 10 points.")
+elif alien_color == "red":
+    print("You earned 15 points.")
