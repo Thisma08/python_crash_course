92b43c35317548091c78a60454ded6028b37d74d
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-21 14:24:51 +0200

    21/10/23 14:24

---
+++ chapter5_if_statements/more_conditional_tests.py
@@ -0,0 +1,99 @@
+string = "Hello"
+print(f"string = {string}")
+
+print("\nIs string == \"Hello\"? Prediction: True")
+print(string == "Hello")
+
+print("\nIs string == \"Goodbye\"? Prediction: False")
+print(string == "Goodbye")
+
+print("\nIs string != \"Hello\"? Prediction: False")
+print(string != "Hello")
+
+print("\nIs string != \"Goodbye\"? Prediction: True")
+print(string != "Goodbye")
+
+print("\nIs string.lower() == \"hello\"? Prediction: True")
+print(string.lower() == "hello")
+
+print("\nIs string.lower() == \"goodbye\"? Prediction: False")
+print(string.lower() == "goodbye")
+
+number = 10
+print(f"\nnumber = {number}")
+
+print("\nIs number == 10? Prediction: True")
+print(number == 10)
+
+print("\nIs number == 20? Prediction: False")
+print(string == 20)
+
+print("\nIs number != 10? Prediction: False")
+print(number != 10)
+
+print("\nIs number != 20? Prediction: True")
+print(string != 20)
+
+print("\nIs number > 5? Prediction: True")
+print(number > 5)
+
+print("\nIs number > 15? Prediction: False")
+print(number > 15)
+
+print("\nIs number < 15? Prediction: True")
+print(number < 15)
+
+print("\nIs number < 5? Prediction: False")
+print(number < 5)
+
+print("\nIs number >= 5? Prediction: True")
+print(number >= 5)
+
+print("\nIs number >= 15? Prediction: False")
+print(number >= 15)
+
+print("\nIs number >= 10? Prediction: True")
+print(number >= 10)
+
+print("\nIs number <= 15? Prediction: True")
+print(number <= 15)
+
+print("\nIs number <= 5? Prediction: False")
+print(number <= 5)
+
+print("\nIs number <= 10? Prediction: True")
+print(number <= 10)
+
+number1 = 10
+number2 = 20
+print(f"\nnumber1 = {number1}, number2 = {number2}")
+
+print("\nIs number1 > 5 and number2 > 15? Prediction: True")
+print(number1 > 5 and number2 > 15)
+
+print("\nIs number1 < 5 and number2 > 15? Prediction: False")
+print(number1 < 5 and number2 > 15)
+
+print("\nIs number1 > 5 or number2 < 15? Prediction: True")
+print(number1 > 5 or number2 < 15)
+
+print("\nIs number1 < 5 or number2 < 15? Prediction: False")
+print(number1 < 5 or number2 < 15)
+
+items_list = ["item1", "item2", "item3"]
+print(f"\nitems_list = {items_list}")
+
+print("\nIs \"item2\" in items_list? Prediction: True")
+print("item2" in items_list)
+
+print("\nIs \"item58\" in items_list? Prediction: False")
+print("item58" in items_list)
+
+print("\nIs \"item58\" not in items_list? Prediction: True")
+print("item58" not in items_list)
+
+print("\nIs \"item2\" not in items_list? Prediction: False")
+print("item2" not in items_list)
+
+
+
