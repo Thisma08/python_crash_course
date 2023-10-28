b67287623558cd1d8412d79bc1631166da059e26
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-28 11:37:07 +0200

    28/10/23 11:37

---
+++ chapter6_dictionaries/person.py
@@ -0,0 +1,6 @@
+person = {'first_name': 'Mathis', 'last_name': 'Mahaux', 'age': 23, 'city': 'Thulin'}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
