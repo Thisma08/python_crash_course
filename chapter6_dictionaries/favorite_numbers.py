9ec2f4fb3fb9cd62dc232539f3f318081608f8ba
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-28 11:48:33 +0200

    28/10/23 11:48

---
+++ chapter6_dictionaries/favorite_numbers.py
@@ -0,0 +1,11 @@
+favorite_numbers = {
+    'Mathis': 8,
+    'Alice': 42,
+    'Marc': 23,
+    'Claire': 67
+}
+
+print(f"Mathis's favorite number is {favorite_numbers['Mathis']}")
+print(f"Alice's favorite number is {favorite_numbers['Alice']}")
+print(f"Claire's favorite number is {favorite_numbers['Claire']}")
+print(f"Marc's favorite number is {favorite_numbers['Marc']}")
---
+++ chapter6_dictionaries/glossary.py
@@ -0,0 +1,10 @@
+glossary = {
+    'for': 'Creates a "for" loop (Known amount of iterations)',
+    'while': 'Creates a "while" loop (Unknown amount of iterations)',
+    'print': 'Prints a statement or the value of a variable',
+    'in': 'Used to loop through the elements of a list (To use with a "for" loop)',
+    'sort()': 'Sorts a list'
+}
+
+for item in glossary:
+    print(item + ": " + glossary[item])
